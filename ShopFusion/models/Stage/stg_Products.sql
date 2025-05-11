2dc2b77e9debebdfdd29af4d76461aa19f8f00ae
Michael <59081538+Delta483711@users.noreply.github.com>
2025-05-11 18:43:30 +0100

    Adding the product Model (#2)

---
+++ ShopFusion/models/Stage/stg_Products.sql
@@ -0,0 +1,15 @@
+-- Stage model for Product table 
+WITH Products AS 
+(
+SELECT
+    product_id AS ProductId,
+    name AS ProductName,
+    category_id AS CategoryId,
+    brand_id AS BrandId,
+    price,
+    created_at as ProductCreationDate
+FROM 
+    public.products
+)
+
+SELECT * FROM Products
