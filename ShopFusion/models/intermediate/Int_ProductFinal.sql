5bd4ac7d00513dc4055568db23c27c1679ce8d08
Delta <Michael483711@outlook.com>
2025-05-14 20:12:43 +0100

    Add new Int Model

---
+++ ShopFusion/models/intermediate/Int_ProductFinal.sql
@@ -0,0 +1,17 @@
+
+
+SELECT 
+    P.ProductId,
+    P.ProductName,
+    B.BrandId,
+    B.BrandName,
+    C.CategoryId,
+    C.CategoryName
+FROM
+   {{ref('stg_Products')}} P
+LEFT JOIN 
+    {{ref('stg_Brand')}} B
+        on P.BrandId = B.BrandId
+LEFT JOIN 
+    {{ref('stg_Category')}} C
+        on P.CategoryId = C.CategoryId
