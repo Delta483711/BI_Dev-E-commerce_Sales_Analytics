8768411697d96a1f642535bf9d7c564929f71f6f
Michael <59081538+Delta483711@users.noreply.github.com>
2025-05-16 09:10:36 +0100

    Add int customer final (#12)
    * Create int_CustomerFinal.sql
    
    * Add Webtraffic to customer table

---
+++ ShopFusion/models/intermediate/int_CustomerFinal.sql
@@ -0,0 +1,20 @@
+-- Create table for customer final --
+
+
+SELECT
+    C.CustomerId,
+    C.CustomerName,
+    C.CustomerReference,
+    C.CreationDate,
+    C.Region,
+    C.Gender,
+    WT.VisitId,
+    WT.VisitTimestamp,
+    WT.SessionId,
+    WT.ReferralSource
+FROM 
+    {{ref('stg_Customer')}} C
+LEFT JOIN
+    {{ref('stg_WebTraffic')}} WT 
+        ON WT.CustomerId = C.CustomerId
+
